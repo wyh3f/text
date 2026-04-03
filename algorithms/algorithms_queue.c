69e62f40a3e815caa0dca7d206f8d07edf38beb4
wyh3f <wyiheng1@outlook.com>
2026-04-03 15:13:28 +0800

    添加了队列文件

---
+++ algorithms/algorithms_queue.c
@@ -0,0 +1,5 @@
+#include <algorithms/algorithms_queue.h>
+
+
+
+
---
+++ algorithms/algorithms_queue.h
@@ -0,0 +1,17 @@
+#ifndef ALGORITHMS_QUEUE_H_
+#define ALGORITHMS_QUEUE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <string.h>
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
